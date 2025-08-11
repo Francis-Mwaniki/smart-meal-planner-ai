b32f40be2b4fd2b35525f7555446239ed6d7c547
Franc-dev <francismwanik254@gmail.com>
2025-08-11 16:29:30 +0300

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,15 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
+  
+  // Disable static generation completely
+  trailingSlash: false,
+  generateBuildId: async () => {
+    return 'build-' + Date.now()
+  },
+  
+  // Force all pages to be dynamic
+  staticPageGenerationTimeout: 0,
 }
 
 export default nextConfig
